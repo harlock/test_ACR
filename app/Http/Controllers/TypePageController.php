13c133aa54c417607618088dd6e01310ea0f8e4b
Harlock <iscprimerocesar@gmail.com>
2021-05-04 12:35:29 -0500

    migration example

---
+++ app/Http/Controllers/TypePageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypePage;
+use Illuminate\Http\Request;
+
+class TypePageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TypePage  $typePage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypePage $typePage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TypePage  $typePage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TypePage $typePage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TypePage  $typePage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypePage $typePage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TypePage  $typePage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypePage $typePage)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_05_04_172819_create_pages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string("description");
+            $table->integer("view_counter");
+            $table->bigInteger("type_page_id")->unsigned();
+            $table->foreign("type_page_id")
+                ->references("id")
+                ->on("type_pages");
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
