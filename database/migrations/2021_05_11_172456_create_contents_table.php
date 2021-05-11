d7e9155d0ed68d6ab53a244f7038771d9d62f2dc
Queen <reynis.9gonazalez@gmail.com>
2021-05-11 12:29:33 -0500

    migration of Contents

---
+++ database/migrations/2021_05_11_172456_create_contents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->String("text");
+            $table->int("position");
+            $table->foreign("project_id")
+            ->references("id")
+                ->on("project_id");
+            $table->Foreign("project_id");
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
