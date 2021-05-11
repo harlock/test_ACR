7e7f844f7720c6073a71dc595189d731847b55b2
fer vargas <fvargs33@gmail.com>
2021-05-11 12:24:57 -0500

    mig

---
+++ database/migrations/2021_05_11_172326_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string("video");
+            $table->string("description");
+            $table->bigInteger("position");
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign("type_id")
+                ->references("id")
+                ->on("pages");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
