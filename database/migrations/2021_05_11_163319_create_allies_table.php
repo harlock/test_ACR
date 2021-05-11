8626bed0c2d3826b98e8d3c64f3907d809323eb8
Queen <reynis.9gonazalez@gmail.com>
2021-05-11 11:41:36 -0500

    migration of allies

---
+++ database/migrations/2021_05_11_163319_create_allies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('allies', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("image");
+            $table->String("telephone");
+            $table->softDeletes() ;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('allies');
+    }
+}
