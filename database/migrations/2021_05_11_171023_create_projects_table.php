89d3c4dda0c04951c91e8df44c07f4f26d2617e2
alfa.alx110@gmail.com <alfa.alx110@gmail.com>
2021-05-11 12:12:24 -0500

    Migration Project

---
+++ database/migrations/2021_05_11_171023_create_projects_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("description");
+            $table->integer("view_counter");
+            $table->integer("enabled");
+            $table->bigInteger("type_project_id")->unsigned();
+            $table->foreign("type_project_id")
+                ->references("id")
+                ->on("type_projects");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
