d13410c429db6fbeae527be21d5528273bc13f10
Queen <reynis.9gonazalez@gmail.com>
2021-05-11 12:22:39 -0500

    migration of SocialNetwork

---
+++ database/migrations/2021_05_11_171120_create_social_networks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocialNetworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_networks', function (Blueprint $table) {
+            $table->id();
+          
+            $table->string("address");
+            $table->bigInteger("ally_id")->unsigned();
+            $table->foreign("ally_id")
+            ->references("id")
+                ->on("ally");
+            $table->bigInteger("social_network_type_id")->unsigned();
+            $table->foreign("social_network_type_id")
+            ->references("id")
+                ->on("social_network_type_id");
+            $table->timestamps();
+            $table->softDeletes() 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_networks');
+    }
+}
